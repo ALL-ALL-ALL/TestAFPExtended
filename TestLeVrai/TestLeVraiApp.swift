7e7159ea7f4da1d63d1e0de6ba24cf0298a4fb0e
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-05-16 09:37:53 +0200

    Initial Commit

---
+++ TestLeVrai/TestLeVraiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLeVraiApp.swift
+//  TestLeVrai
+//
+//  Created by  Ixart on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestLeVraiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
